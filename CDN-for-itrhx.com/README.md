28ae6b3826b0bcf1b1e0decc0f4b57fdc0c8aab7
YS-THX <1522138135@qq.com>
2021-09-14 23:09:32 +0800

    BAK

---
+++ CDN-for-itrhx.com/README.md
@@ -0,0 +1,8 @@
+# CDN-for-itrhx.com
+jsDeliver CDN-for-itrhx.com
+
+# 教程
+
+https://www.itrhx.com/2019/02/10/A18-free-cdn/
+
+https://itrhx.blog.csdn.net/article/details/86936453
---
+++ CDN-for-itrhx.com/js/FunnyTitle.js
@@ -0,0 +1,17 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.4/images/funny.ico");
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/TRHX/CDN-for-itrhx.com@3.0.4/images/favicon.ico");
+        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
